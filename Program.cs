fb2a840dde86644fe47aca1f0850ce753ecdf673
xalenoxjeri-max <xalenox.jeri@gmail.com>
2025-11-13 13:01:30 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int numero;
+bool esPrimo = true;
+
+Console.WriteLine("ingresa un numero");
+numero = int.Parse(Console.ReadLine());
+
+if (numero <= 1)
+{
+    esPrimo = false;
+}
+else
+{
+    for (int i = 2; i <= numero / 2; i++)
+    {
+        if (numero % i == 0)
+        {
+            esPrimo = false;
+            break;
+        }
+    }
+}
+if (esPrimo)
+    Console.WriteLine($" {numero} es un numero primo.");
+else
+    Console.WriteLine($"{numero} no es un numero primo.");
+
