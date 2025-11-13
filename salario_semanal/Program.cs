928f18f06efccd1307a85f9cb2a29901c5b09fe1
yalexia <xalenox.jeri@gmail.com>
2025-11-13 12:22:39 -0500

    Agregar archivos de proyecto.

---
+++ salario_semanal/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ingrese el salario por hora");
+double salarioHora = double.Parse(Console.ReadLine());
+
+Console.WriteLine("ingrese el numero de horas trabajadas por semana");
+double horasTrabajadas = double.Parse(Console.ReadLine());
+
+double sueldoTotal;
+if (horasTrabajadas <= 40)
+{
+    sueldoTotal = salarioHora * horasTrabajadas;
+}
+else
+{
+    double horasExtras = horasTrabajadas - 40;
+    double salarioExtra = salarioHora * 1.5;
+    sueldoTotal = (salarioHora * 40) + (salarioExtra * horasExtras);
+}
+Console.WriteLine($"el sueldo total semanal es: S/{sueldoTotal}");
